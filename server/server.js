e08581d93e980ab5ce5df20c963e78714783d406
Seaw Ker Boon <seawkerboon@gmail.com>
2019-02-12 14:24:36 +0800

    Add connect and disconnect event handler

--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
 require('./config/config');
 
+const http = require('http');
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
-
 const express = require('express');
+const socketIO = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 const port = process.env.PORT;
 
 app.use(express.static(publicPath));      //express.static (take the absolute path of the folder that you want to serve)
 
+io.on('connection', (socket) => {
+    console.log('Connected to server');
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log('Disconnect from server');
+    })
+});
+
+server.listen(port, () => {
     console.log(`Started on port ${port}`);
 })
